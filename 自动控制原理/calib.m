f21dc3ddefc7847faf9b66a5007977a4b3015806
kumiko <156634184+kumikosjtu@users.noreply.github.com>
2025-01-15 12:17:28 +0800

    更新自控

--- 自动控制原理/calib.m
+++ 自动控制原理/calib.m
@@ -1,12 +1,11 @@
 s = tf('s');
-% G = 20 / (s * (s / 2 + 1));
-% a = 10;
-% T = 0.02834;
+K=45/4;
+bode(K/(s+1)/(0.5*s+1)/(2*s+1))
+% G0 = 10 / ((s/2 + 1) * (s/15 + 1));
+% a = 1.0700;
+% T = 1000;
 % Gc = (a * T * s + 1) / (T * s + 1);
-% Gp = G * Gc;
-% bode(G, Gc, Gp);
-% legend('G', 'Gc', 'Gp');
+% G = G0 * Gc;
+% bode(G0, Gc, G);
+% legend('G0', 'Gc', 'G');
 % grid on;
-G = 16 / (s*(s+2));
-bode(G);
-grid on;
